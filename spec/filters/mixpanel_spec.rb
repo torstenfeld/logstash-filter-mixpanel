f48997b605f57e3c6e891f2ba1c4768af056419d
Torsten Feld <torsten@torsten-feld.de>
2015-06-06 16:36:40 +0200

    create test setup and fake test for testing the test xD

--- spec/filters/mixpanel_spec.rb
+++ spec/filters/mixpanel_spec.rb
@@ -1,20 +1,42 @@
 require File.absolute_path(File.join(File.dirname(__FILE__), '../../spec/spec_helper'))
 require File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/logstash/filters/mixpanel'))
+require 'mixpanel_client'
+require 'mixpanel-ruby'
+require 'ffaker'
+require 'base64'
 
-describe LogStash::Filters::Example do
-  describe "Set to Hello World" do
+
+describe LogStash::Filters::Mixpanel do
+  before(:all) do
+    @mp = Mixpanel::Tracker.new(ENV['MP_PROJECT_TOKEN'])
+
+    @user_id = FFaker::Guid.guid
+    @user_ip = FFaker::Internet.ip_v4_address
+    @user_data = {
+        :$first_name => FFaker::NameDE.first_name,
+        :$last_name => FFaker::NameDE.last_name,
+        :$email => FFaker::Internet.safe_email
+    }
+    @mp.people.set(@user_id, @user_data, ip=@user_ip)
+    @mp.track(@user_id, 'user created')
+  end
+
+
+  describe 'first test' do
     let(:config) do <<-CONFIG
       filter {
-        example {
-          message => "Hello World"
+        mixpanel {
+          api_key => '123'
+          api_secret => '123'
+          where => '123'
         }
       }
     CONFIG
     end
 
-    sample("message" => "some text") do
-      expect(subject).to include("message")
-      expect(subject['message']).to eq('Hello World')
+    sample("message" => "test") do
+      expect(subject).to include('message')
+      expect(subject['message']).to eq('test')
     end
   end
 end
